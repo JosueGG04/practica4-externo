9e3a867b3a125fbf861ea8e96e4b1ce66f7dedc1
Josue Garcia <jdgg0001@ce.pucmm.edu.do>
2024-02-26 22:09:32 -0400

    cambio de puerto de la aplicacion 1

--- app1/src/main/java/org/example/Main.java
+++ app1/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7070);
+        Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> {
             ctx.html("<h1>Esta es una aplicacion</h1>");
         });
